a295af6bcc02e60ae299a1a180c724a4de2945c4
nexorqk <pkms2a10@gmail.com>
2024-06-26 20:49:24 +0300

    feat: auth crud

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,76 @@
+import express from "express";
+
+import { createUser, getUserByUsername } from "../db/user";
+import { authentication, random } from "../helpers";
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByUsername(username).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByUsername(username);
+
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
